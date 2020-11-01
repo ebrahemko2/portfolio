74ae6fa060e720805abf0b5ff1a7e7d39b54f8ea
Ibrahim Ashour <43494882+ebrahemko2@users.noreply.github.com>
2020-11-02 01:27:04 +0200

    first version

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // Form label appear when you start typing in inputs 
+    $(".contact-form .form-control").on("focusin", function() {
+        $(this).siblings().css({"opacity": 1, "animation-name": "displayLabel"}, 500);
+        $(this).attr("placeholder", "");
+    });
+    $(".contact-form .form-control").on("focusout", function() {
+        $(this).siblings().css({"opacity": 0, "animation-name": "none"}, 500);
+        $(this).attr("placeholder", $(this).siblings().text());
+    });
+
+    // scroll to top arrow 
+    $("#toTop").click(function () {
+        $("html, body").scrollTop(0);
+     });
+});
+
+
